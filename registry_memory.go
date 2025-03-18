1ee82f585441dbf53dc68251ae68ce54d7743170
goliatone <140582+goliatone@users.noreply.github.com>
2025-03-18 16:54:16 -0700

    add: memory registry

---
+++ registry_memory.go
@@ -0,0 +1,43 @@
+package job
+
+import (
+	"fmt"
+	"sync"
+)
+
+type memoryRegistry struct {
+	mx   sync.RWMutex
+	jobs map[string]Task
+}
+
+func (r *memoryRegistry) Add(job Task) error {
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	id := job.GetID()
+	if _, exists := r.jobs[id]; exists {
+		return fmt.Errorf("job with ID %s already exists", id)
+	}
+
+	r.jobs[id] = job
+	return nil
+}
+
+func (r *memoryRegistry) Get(id string) (Task, bool) {
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	job, ok := r.jobs[id]
+	return job, ok
+}
+
+func (r *memoryRegistry) List() []Task {
+	r.mx.Lock()
+	defer r.mx.Unlock()
+
+	jobs := make([]Task, 0, len(r.jobs))
+	for _, job := range r.jobs {
+		jobs = append(jobs, job)
+	}
+	return jobs
+}
