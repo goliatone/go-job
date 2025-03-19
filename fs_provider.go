6cdf5e4e893d2fe94079ba7a2e859bf476e4eda7
goliatone <140582+goliatone@users.noreply.github.com>
2025-03-18 18:29:58 -0700

    add: fs provider

---
+++ fs_provider.go
@@ -0,0 +1,94 @@
+package job
+
+import (
+	"context"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+type FileSystemSourceProvider struct {
+	rootDir string
+	fs      fs.FS
+}
+
+func NewFileSystemSourceProvider(rootDir string, fss ...fs.FS) *FileSystemSourceProvider {
+	fsys := os.DirFS(rootDir)
+	if len(fss) > 0 {
+		fsys = fss[0]
+	}
+	return &FileSystemSourceProvider{
+		rootDir: rootDir,
+		fs:      fsys,
+	}
+}
+
+func (p *FileSystemSourceProvider) GetScript(path string) ([]byte, error) {
+	file, err := p.fs.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", path, err)
+	}
+	defer file.Close()
+
+	info, err := file.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("failed to stat file %s: %w", path, err)
+	}
+
+	content := make([]byte, info.Size())
+	if _, err := file.Read(content); err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", path, err)
+	}
+	return content, nil
+}
+
+func (p *FileSystemSourceProvider) ListScripts(ctx context.Context) ([]ScriptInfo, error) {
+	var scripts []ScriptInfo
+
+	err := fs.WalkDir(p.fs, ".", func(path string, d fs.DirEntry, err error) error {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		info, err := d.Info()
+		if err != nil {
+			return fmt.Errorf("failed ot get file info for %s: %w", path, err)
+		}
+
+		content := make([]byte, info.Size())
+		file, err := p.fs.Open(path)
+		if err != nil {
+			return fmt.Errorf("failed to read file %s: %w", path, err)
+		}
+		defer file.Close()
+
+		if _, err := file.Read(content); err != nil {
+			return fmt.Errorf("failed to read file %s: %w", path, err)
+		}
+
+		absPath := path
+		if p.rootDir != "" {
+			absPath = filepath.Join(p.rootDir, path)
+		}
+		scripts = append(scripts, ScriptInfo{
+			ID:      filepath.Base(path),
+			Path:    absPath,
+			Content: content,
+		})
+
+		return nil
+	})
+
+	return scripts, err
+}
