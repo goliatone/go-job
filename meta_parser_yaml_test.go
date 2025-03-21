9e5c9fa020cc13b89dabe61e0a256d58fb984728
goliatone <140582+goliatone@users.noreply.github.com>
2025-03-20 21:41:19 -0700

    add: support JS block comments

--- meta_parser_yaml_test.go
+++ meta_parser_yaml_test.go
@@ -139,6 +139,51 @@ console.log("Running script...");`)
 	assert.Equal(t, "\nconsole.log(\"Running script...\");", script)
 }
 
+func TestYAMLMetadataParser_Parse_JS_block(t *testing.T) {
+	parser := job.NewYAMLMetadataParser()
+	content := []byte(`
+/** config
+ * schedule: "0 12 * * *"
+ * timeout: 300s
+ * retries: 30
+ * debug: true
+ * run_once: true
+ * script_type: shell
+ * transaction: true
+ * env:
+ *  APP_NAME: test
+ *  APP_PORT: 1234
+ * metadata:
+ *  test1: test
+ *  test2: 2
+*/
+
+console.log("Running script...");`)
+
+	env := map[string]string{
+		"APP_NAME": "test",
+		"APP_PORT": "1234",
+	}
+	meta := map[string]any{
+		"test1": "test",
+		"test2": 2,
+	}
+	config, script, err := parser.Parse(content)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "0 12 * * *", config.Schedule)
+	// "300s" parsed correctly
+	assert.Equal(t, 300, int(config.Timeout.Seconds()))
+	assert.Equal(t, 30, config.Retries)
+	assert.Equal(t, true, config.Debug)
+	assert.Equal(t, true, config.RunOnce)
+	assert.Equal(t, true, config.Transaction)
+	assert.Equal(t, "shell", config.ScriptType)
+	assert.Equal(t, env, config.Env)
+	assert.Equal(t, meta, config.Metadata)
+	assert.Equal(t, "\nconsole.log(\"Running script...\");", script)
+}
+
 func TestYAMLMetadataParser_Parse_NoMetadata(t *testing.T) {
 	parser := job.NewYAMLMetadataParser()
 	content := []byte("echo 'No metadata'")
