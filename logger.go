36355128d9f04f3722ee77cdb9333691f9b71855
goliatone <140582+goliatone@users.noreply.github.com>
2025-04-13 20:45:50 -0700

    refactor: use unified logger

---
+++ logger.go
@@ -0,0 +1,32 @@
+package job
+
+import "fmt"
+
+type Logger interface {
+	Debug(format string, args ...any)
+	Info(format string, args ...any)
+	Error(format string, args ...any)
+}
+
+var LoggerEnabled = false
+
+type defaultLogger struct {
+}
+
+func (d *defaultLogger) Debug(format string, args ...any) {
+	if LoggerEnabled {
+		fmt.Printf("[DEBUG] "+format+"\n", args...)
+	}
+}
+
+func (d *defaultLogger) Info(format string, args ...any) {
+	if LoggerEnabled {
+		fmt.Printf("[INFO] "+format+"\n", args...)
+	}
+}
+
+func (d *defaultLogger) Error(format string, args ...any) {
+	if LoggerEnabled {
+		fmt.Printf("[ERROR] "+format+"\n", args...)
+	}
+}
