664d02a04f727fa4868b67c6440dc48aafdd0129
goliatone <140582+goliatone@users.noreply.github.com>
2025-05-13 10:39:04 -0700

    refactor: make set fetch public

--- js_runner.go
+++ js_runner.go
@@ -78,7 +78,7 @@ func (e *JSEngine) Execute(ctx context.Context, msg *ExecutionMessage) error {
 		buffer.Enable(vm)
 		console.Enable(vm)
 
-		if ferr := e.SetupFetch(vm); ferr != nil {
+		if ferr := e.setupFetch(vm); ferr != nil {
 			configErrCh <- ferr
 			return
 		}
