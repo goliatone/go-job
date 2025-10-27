c1ce02b88c7588a714bbd7bea0cc8c13e4978701
goliatone <140582+goliatone@users.noreply.github.com>
2025-10-26 21:11:47 -0700

    add: task provider implementation

--- runner.go
+++ runner.go
@@ -9,10 +9,12 @@ type Runner struct {
 	mx       sync.RWMutex
 	registry Registry
 
-	parser       MetadataParser
-	errorHandler func(Task, error)
-	taskCreators []TaskCreator
-	logger       Logger
+	parser            MetadataParser
+	errorHandler      func(Task, error)
+	taskCreators      []TaskCreator
+	logger            Logger
+	taskIDProvider    TaskIDProvider
+	taskEventHandlers []TaskEventHandler
 }
 
 func NewRunner(opts ...Option) *Runner {
@@ -43,17 +45,58 @@ func NewRunner(opts ...Option) *Runner {
 
 func (r *Runner) Start(ctx context.Context) error {
 	for _, make := range r.taskCreators {
+		if err := ctx.Err(); err != nil {
+			r.handleContextCancellation(err)
+			return err
+		}
+
 		tasks, err := make.CreateTasks(ctx)
 		if err != nil {
+			if ctxErr := ctx.Err(); ctxErr != nil {
+				r.handleContextCancellation(ctxErr)
+				return ctxErr
+			}
+
 			r.errorHandler(nil, err)
+			r.emitTaskEvent(TaskEvent{
+				Type: TaskEventRegistrationFailed,
+				Err:  err,
+			})
 			continue
 		}
+
 		for _, task := range tasks {
+			if err := ctx.Err(); err != nil {
+				r.handleContextCancellation(err)
+				return err
+			}
+
 			if err := r.registry.Add(task); err != nil {
 				r.errorHandler(task, err)
+				r.emitTaskEvent(TaskEvent{
+					Type:       TaskEventRegistrationFailed,
+					TaskID:     task.GetID(),
+					ScriptPath: taskScriptPath(task),
+					Task:       task,
+					Err:        err,
+				})
+				continue
 			}
+
+			r.emitTaskEvent(TaskEvent{
+				Type:       TaskEventRegistered,
+				TaskID:     task.GetID(),
+				ScriptPath: taskScriptPath(task),
+				Task:       task,
+			})
 		}
 	}
+
+	if err := ctx.Err(); err != nil {
+		r.handleContextCancellation(err)
+		return err
+	}
+
 	return nil
 }
 
@@ -63,4 +106,76 @@ func (r *Runner) Stop(_ context.Context) error {
 
 func (r *Runner) RegisteredTasks() []Task {
 	return r.registry.List()
+}
+
+func (r *Runner) emitTaskEvent(event TaskEvent) {
+	for _, handler := range r.taskEventHandlers {
+		handler(event)
+	}
+}
+
+func (r *Runner) handleContextCancellation(err error) {
+	if err == nil {
+		return
+	}
+	r.errorHandler(nil, err)
+	r.emitTaskEvent(TaskEvent{
+		Type: TaskEventRegistrationFailed,
+		Err:  err,
+	})
+}
+
+func (r *Runner) attachTaskCreatorOptions(creator TaskCreator) {
+	if creator == nil {
+		return
+	}
+
+	if r.taskIDProvider != nil {
+		if consumer, ok := creator.(TaskIDProviderAware); ok {
+			consumer.SetTaskIDProvider(r.taskIDProvider)
+		}
+	}
+
+	if emitter, ok := creator.(TaskEventEmitter); ok {
+		for _, handler := range r.taskEventHandlers {
+			emitter.AddTaskEventHandler(handler)
+		}
+	}
+}
+
+func (r *Runner) propagateTaskEventHandler(handler TaskEventHandler) {
+	if handler == nil {
+		return
+	}
+
+	for _, creator := range r.taskCreators {
+		if emitter, ok := creator.(TaskEventEmitter); ok {
+			emitter.AddTaskEventHandler(handler)
+		}
+	}
+}
+
+func (r *Runner) propagateTaskIDProvider() {
+	if r.taskIDProvider == nil {
+		return
+	}
+
+	for _, creator := range r.taskCreators {
+		if consumer, ok := creator.(TaskIDProviderAware); ok {
+			consumer.SetTaskIDProvider(r.taskIDProvider)
+		}
+	}
+}
+
+func taskScriptPath(task Task) string {
+	if task == nil {
+		return ""
+	}
+	type pathAware interface {
+		GetPath() string
+	}
+	if v, ok := task.(pathAware); ok {
+		return v.GetPath()
+	}
+	return ""
 }
