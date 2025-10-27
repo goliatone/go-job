667ec6288205865c5db927a5adbd607271f2c139
goliatone <140582+goliatone@users.noreply.github.com>
2025-10-26 21:26:26 -0700

    add: better base logging

--- base_task.go
+++ base_task.go
@@ -2,6 +2,7 @@ package job
 
 import (
 	"context"
+	"time"
 
 	"github.com/goliatone/go-command"
 )
@@ -33,9 +34,28 @@ func (j *baseTask) GetHandler() func() error {
 			Parameters: make(map[string]any),
 		}
 		emsg.Parameters["script"] = j.scriptContent
-		j.logger.Debug("executing task", "engine", j.engine.Name())
 
-		return j.engine.Execute(ctx, emsg)
+		logger := j.taskLogger()
+		baseArgs := []any{"task_id", j.id, "script_path", j.scriptPath}
+		if j.engine != nil {
+			baseArgs = append(baseArgs, "engine", j.engine.Name())
+		}
+
+		logger.Debug("task execution started", baseArgs...)
+
+		start := time.Now()
+		err := j.engine.Execute(ctx, emsg)
+		duration := time.Since(start)
+
+		durationArgs := append(append([]any{}, baseArgs...), "duration", duration)
+
+		if err != nil {
+			logger.Error("task execution failed", append(durationArgs, "error", err)...)
+			return err
+		}
+
+		logger.Info("task execution completed", durationArgs...)
+		return nil
 	}
 }
 
@@ -95,6 +115,28 @@ func NewBaseTask(
 		scriptContent: scriptContent,
 		engine:        engine,
 		config:        config,
-		logger:        &defaultLogger{},
+		logger:        newStdLoggerProvider().GetLogger("job:task"),
 	}
+}
+
+func (j *baseTask) taskLogger() Logger {
+	logger := j.logger
+	if logger == nil {
+		logger = newStdLoggerProvider().GetLogger("job:task")
+	}
+
+	fields := map[string]any{
+		"task_id":     j.id,
+		"script_path": j.scriptPath,
+	}
+
+	if j.engine != nil {
+		fields["engine"] = j.engine.Name()
+	}
+
+	if fl, ok := logger.(FieldsLogger); ok {
+		return fl.WithFields(fields)
+	}
+
+	return logger
 }
