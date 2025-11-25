34c9cf5e3ef3e7b95c5b9ee47475e3bdad02da7a
goliatone <140582+goliatone@users.noreply.github.com>
2025-11-25 14:02:45 -0800

    add: merge config

--- config_merge.go
+++ config_merge.go
@@ -33,11 +33,26 @@ func mergeConfigDefaults(base, override Config) Config {
 	if override.ExitOnError {
 		result.ExitOnError = true
 	}
+	if override.MaxConcurrency != 0 {
+		result.MaxConcurrency = override.MaxConcurrency
+	}
 	if override.ScriptType != "" {
 		result.ScriptType = override.ScriptType
 	}
 	if override.Transaction {
 		result.Transaction = true
+	}
+	if override.Backoff.Strategy != "" {
+		result.Backoff.Strategy = override.Backoff.Strategy
+	}
+	if override.Backoff.Interval != 0 {
+		result.Backoff.Interval = override.Backoff.Interval
+	}
+	if override.Backoff.MaxInterval != 0 {
+		result.Backoff.MaxInterval = override.Backoff.MaxInterval
+	}
+	if override.Backoff.Jitter {
+		result.Backoff.Jitter = true
 	}
 	if override.Metadata != nil {
 		result.Metadata = override.Metadata
@@ -45,6 +60,26 @@ func mergeConfigDefaults(base, override Config) Config {
 	if override.Env != nil {
 		result.Env = override.Env
 	}
+	if override.Backoff.Strategy != "" || override.Backoff.Interval != 0 || override.Backoff.MaxInterval != 0 || override.Backoff.Jitter {
+		result.Backoff = mergeBackoffDefaults(base.Backoff, override.Backoff)
+	}
 
+	return result
+}
+
+func mergeBackoffDefaults(base, override BackoffConfig) BackoffConfig {
+	result := base
+	if override.Strategy != "" {
+		result.Strategy = override.Strategy
+	}
+	if override.Interval != 0 {
+		result.Interval = override.Interval
+	}
+	if override.MaxInterval != 0 {
+		result.MaxInterval = override.MaxInterval
+	}
+	if override.Jitter {
+		result.Jitter = true
+	}
 	return result
 }
