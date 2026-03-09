a8d639a2840f8dc383c3ba80164be5b4c2b08afb
goliatone <140582+goliatone@users.noreply.github.com>
2026-03-09 16:57:39 -0700

    add: queue resolver support options

--- queue/command/resolver.go
+++ queue/command/resolver.go
@@ -4,10 +4,15 @@ import "github.com/goliatone/go-command"
 
 // QueueResolver registers go-command handlers with the queue registry.
 func QueueResolver(reg *Registry) command.Resolver {
+	return QueueResolverWithOptions(reg)
+}
+
+// QueueResolverWithOptions registers go-command handlers with queue registration options.
+func QueueResolverWithOptions(reg *Registry, opts ...RegisterOption) command.Resolver {
 	return func(cmd any, meta command.CommandMeta, _ *command.Registry) error {
 		if meta.MessageType == "" {
 			return nil
 		}
-		return RegisterCommandByType(reg, cmd, meta)
+		return RegisterCommandByTypeWithOptions(reg, cmd, meta, opts...)
 	}
 }
